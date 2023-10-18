54d01c3568dac115cbd0313043c6c411a24134f2
Denis Zhuravlev <denis-zhuravlev-2017@mail.ru>
2023-10-18 11:59:04 +0500

    готовая верстка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles/index.scss";
+import Parallax from "parallax-js";
+import $ from "jquery";
+import "slick-carousel";
+
+document.addEventListener("DOMContentLoaded", function () {
+  let scene = document.querySelector(".scene");
+  let parallaxInstance = new Parallax(scene, {
+    // relativeInput: true,
+    limitX: 20,
+  });
+  $(".cases__slider").slick({
+    // centerMode: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+  });
+});
